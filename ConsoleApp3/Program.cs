9be9bde456deddcf4000097611cf6efd9a0404d9
gokay <gokay@GOKAY>
2024-12-20 14:37:26 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Lütfen bir sayı giriniz: ");
+int sayi = int.Parse(Console.ReadLine());
+
+if (sayi < 10)
+{
+    Console.WriteLine("Sayı 10'dan küçüktür");
+}
+else if (sayi == 10)  
+{
+    Console.WriteLine("Sayı 10'a eşittir");
+}
+else  
+{
+    Console.WriteLine("Sayı 10'dan büyüktür");
+}
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Sayı tektir.");
+}
